39a28acc83fa986bc080f86a5253d19b75f8255e
Kenneth Hoff <kenneth.98@hotmail.com>
2026-03-03 23:08:42 +0100

    refactor: remove flake-parts

--- flake.nix
+++ flake.nix
@@ -2,54 +2,83 @@
   description = "Nix flake for the .NET Aspire CLI";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
 
-      perSystem = {pkgs, system, ...}: let
-        versions = import ./versions.nix;
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
+        });
+  in {
+    packages = forAllSystems ({pkgs, system}: let
+      versions = import ./versions.nix;
 
-        mkAspire = channel:
-          pkgs.callPackage ./package.nix {
-            inherit system;
-            inherit (versions.${channel}) version;
-            inherit (versions.${channel}) fileVersion;
-            hash = versions.${channel}.hashes.${system};
-          };
-
-        aspire = {
-          stable = mkAspire "stable";
-          staging = mkAspire "staging";
-          dev = mkAspire "dev";
-        };
-      in {
-        packages = {
-          aspire-cli-stable = aspire.stable;
-          aspire-cli-staging = aspire.staging;
-          aspire-cli-dev = aspire.dev;
-          aspire-cli = aspire.stable;
-          default = aspire.stable;
+      mkAspire = channel:
+        pkgs.callPackage ./package.nix {
+          inherit system;
+          inherit (versions.${channel}) version;
+          inherit (versions.${channel}) fileVersion;
+          hash = versions.${channel}.hashes.${system};
         };
 
-        apps.default = {
-          type = "app";
-          program = "${aspire.stable}/bin/aspire";
-        };
-
-        devShells.default = pkgs.mkShell {
-          packages = builtins.attrValues aspire;
-        };
-
-        formatter = pkgs.nixfmt;
+      aspire = {
+        stable = mkAspire "stable";
+        staging = mkAspire "staging";
+        dev = mkAspire "dev";
       };
-    };
+    in {
+      aspire-cli-stable = aspire.stable;
+      aspire-cli-staging = aspire.staging;
+      aspire-cli-dev = aspire.dev;
+      aspire-cli = aspire.stable;
+      default = aspire.stable;
+    });
+
+    apps = forAllSystems ({pkgs, system}: let
+      versions = import ./versions.nix;
+      aspire = pkgs.callPackage ./package.nix {
+        inherit system;
+        inherit (versions.stable) version;
+        inherit (versions.stable) fileVersion;
+        hash = versions.stable.hashes.${system};
+      };
+    in {
+      default = {
+        type = "app";
+        program = "${aspire}/bin/aspire";
+      };
+    });
+
+    devShells = forAllSystems ({pkgs, system}: let
+      versions = import ./versions.nix;
+
+      mkAspire = channel:
+        pkgs.callPackage ./package.nix {
+          inherit system;
+          inherit (versions.${channel}) version;
+          inherit (versions.${channel}) fileVersion;
+          hash = versions.${channel}.hashes.${system};
+        };
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          (mkAspire "stable")
+          (mkAspire "staging")
+          (mkAspire "dev")
+        ];
+      };
+    });
+
+    formatter = forAllSystems ({pkgs, ...}: pkgs.nixfmt);
+  };
 }

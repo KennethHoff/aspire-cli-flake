b33a7843c99cd47a298312e36eb1d9f05938e106
KennethHoff <17124533+KennethHoff@users.noreply.github.com>
2026-06-16 08:19:45 +0000

    chore: update Aspire CLI versions

--- versions.nix
+++ versions.nix
@@ -1,29 +1,29 @@
 {
   stable = {
-    version = "13.4.3-preview.1.26305.13";
-    fileVersion = "13.4.3";
+    version = "13.4.4-preview.1.26314.3";
+    fileVersion = "13.4.4";
     hashes = {
-      x86_64-linux = "sha512-SiXdo+213XG/DKU97e20NtkSQ+Zlf9NRzXc64cUX8U8i7f0rHYja6hWIpnq3RaVLs+uS7C4yrm9oOHqAMiUZFw==";
-      aarch64-linux = "sha512-XPbwK6JpaF9SKRtO0uJj7+WgFwtYFrWL6fpyqiMeEw0S+dhV8s2cYlqbGBTHLpSYT+hBNcWGKp4hxfUQHtqCxw==";
-      aarch64-darwin = "sha512-gAmVVxMvFi50PKaW9ES3X9dZhjN9SmPsaR8dZ1Fq3igPtruBkfjReIcObu2uYCod4NXdOLsMRKNgrR6vLUhCzg==";
+      x86_64-linux = "sha512-Pfwafu8pg9qOTx0Vt4BSN3opsTQ2TFXd2pvG1EwdLe/lKea19c769wOGzMFld66msIMrzWK5HIc+yUG6AdF6Yw==";
+      aarch64-linux = "sha512-P4OL+Tk3Z5IclsZRwqAv15lkPmc297MdIoZL8rANGbZSS2+msyPeifeW3E119sZC2fOZmjuu1kDGepcg7GEgtA==";
+      aarch64-darwin = "sha512-vFX2CM5GAgnJyo7u5qk6Fr8ZULU+7dlTwARb7eRd8qP3imuB4rTvAE8agZrILt6KOR4DVpDNuKMpk99+opoqaA==";
     };
   };
   staging = {
-    version = "13.4.3-preview.1.26305.14";
-    fileVersion = "13.4.3";
+    version = "13.4.4-preview.1.26310.6";
+    fileVersion = "13.4.4";
     hashes = {
-      x86_64-linux = "sha512-Q6KK45aer4w98nNDTE9jiuKuza+BTWFKMzKqXQkRWJCPNeo8Ng4r4ALqw344kOnRxu34dB3UWn7UB8BbE+FLGw==";
-      aarch64-linux = "sha512-14R09FTQYog0uUmzTNH/Agzzh0EAvSfqSsQGycpryiC4cPLEfSgYEwvXHghmEqhwCbFkNpRVxPbmjync4r5uWA==";
-      aarch64-darwin = "sha512-7QT/LTuyCNn4wjNT6ds5pIGsODgPy9tJV8psZSP8AsO5vOsBSWmZ+uKh1H2okx38DfjVnUtwnHSfYmWSHFjJ0g==";
+      x86_64-linux = "sha512-EzwoQG1Qo5QSQsE++GE/1UwyFRJxaHl9RjCUqneUHMQbga+Ml6vn196i3toU83m5GGdVNMEoadMGyOyXpbd17g==";
+      aarch64-linux = "sha512-EZf52Lb0cLczJpBaNFSSCKg425n8PkVVeqfAVsfoG2RZyS8+VbSsLs9kj0NutjamQ6SEsLEnQvTknYUne/ZpAA==";
+      aarch64-darwin = "sha512-G1PTL+rV3eRzhDCIjsPObAIMK098jwd60Yu0C46WdnccV9Ec7KswOB2kO8Ev3C7D5yOYFxOOuMWQlNKqB0TBXw==";
     };
   };
   dev = {
-    version = "13.5.0-preview.1.26308.5";
-    fileVersion = "13.5.0-preview.1.26308.5";
+    version = "13.5.0-preview.1.26315.6";
+    fileVersion = "13.5.0-preview.1.26315.6";
     hashes = {
-      x86_64-linux = "sha512-NTprKJUGKWGAA41hpsmL+GZTWTZvq4TK2UPBK5+ey/61g7ghRQw7Lx32Q/o3vKsZ2jTb+x9s2QQqOZMA18uJAA==";
-      aarch64-linux = "sha512-CWxxj6x+3MZ50T72iH3z13ZsehD7pxcBVFuXeSv8sAHNjtyFiOKG9VqBt58n12S1HbLyqlndcmcgoiYYNFbJwg==";
-      aarch64-darwin = "sha512-ubTuH+Lj5A+r4Kk9YkuhG2vKK71gkpTgkzuw+oHcJL+JEzFI211S/q+WXImYkID3NphFmH2SFCSMqKgyeHqawQ==";
+      x86_64-linux = "sha512-GE4er2VabC+I1E/lC/Fp13IYCt30mt1mUlFkE1cdl14fj4Fo8YjMdUt246Fy7URxQujerVMaRvs9hbcJ5Je9Jg==";
+      aarch64-linux = "sha512-0wtMrimz0zTjhnoAH7KHm6UjdjippKibj5WI6fVqfDeDRMYSc0RqOUqXycZHML0wFDMMQ+MfYiQe0kBcU4n4wg==";
+      aarch64-darwin = "sha512-yuFv1XU5m7wMdbjt9SMgublG6jE/2fAyaLuiRBMBrDxGd2STHlQPmz/tsqjth+Tw3AFfzsuHNyqBRyjFyBWvug==";
     };
   };
 }
